6dc5f2a2175b4d68d719a249ba2b92d819330deb
Ivan Zdravkovic <izdravkovic@localhost.localdomain>
2018-10-15 13:27:08 +0200

    exercise-5.5

---
+++ src/parts/PartInfo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ part.title }}</h1>
+    <div>
+      {{ part.description }}
+    </div>
+  </div>
+</template>
+
+<script>
+// import parts from '../data/parts';
+
+export default {
+  name: 'PartInfo',
+  data() {
+    return {
+      part: {
+        title: 'Part Title',
+        description: 'Part Description',
+      },
+    };
+  },
+};
+</script>
+
